d227d403572751a37e9b4f8059997ef27c1d4e3b
Alex <alexander.tutunnik@leasoft.org>
2020-07-11 14:28:20 +0300

    Stage 1 development : pre-release capture order function.

--- src/Subscriber/captureOrder.php
+++ src/Subscriber/captureOrder.php
@@ -1,23 +1,94 @@
-<?php declare(strict_types=1);
+<?php
 
 namespace Ginger\EmsPay\Subscriber;
 
-use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenEvent;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Shopware\Core\Checkout\Order\OrderEvents;
+use Shopware\Core\Checkout\Cart\Exception\OrderDeliveryNotFoundException;
+use Shopware\Core\Checkout\Cart\Exception\OrderNotFoundException;
+use Shopware\Core\Checkout\Order\Aggregate\OrderDelivery\OrderDeliveryEntity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\System\StateMachine\Event\StateMachineStateChangeEvent;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use Shopware\Core\Checkout\Order\OrderEntity;
+use Shopware\Core\Framework\Context;
 
-class captureOrder implements EventSubscriberInterface
+
+class captureOrder
 {
-    public static function getSubscribedEvents(): array
-    {
-        // Return the events to listen to as array like this:  <event to listen to> => <method to execute>
-        return [
-            OrderEvents::ORDER_DELIVERY_WRITTEN_EVENT => 'DeliveryTime'
-        ];
+    protected $ginger;
+
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $orderRepository;
+
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $orderTransactionRepository;
+
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $orderDeliveryRepository;
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
+    public function __construct(
+        EntityRepositoryInterface $orderRepository,
+        EntityRepositoryInterface $orderDeliveryRepository
+    ) {
+        $this->orderRepository = $orderRepository;
+        $this->orderDeliveryRepository = $orderDeliveryRepository;
     }
 
-    public function DeliveryTime(EntityWrittenEvent $event): void
+    /**
+     * @throws OrderDeliveryNotFoundException
+     * @throws OrderNotFoundException
+     */
+    public function onOrderDeliveryStateChange(StateMachineStateChangeEvent $event): void
     {
-        print_r("There is still nothing");
+        $orderDeliveryId = $event->getTransition()->getEntityId();
+        $context = $event->getContext();
+
+        /** @var OrderDeliveryEntity|null $orderDelivery */
+        $orderDelivery = $this->orderDeliveryRepository->search(
+            new Criteria([$orderDeliveryId]),
+            $context
+        )->first();
+
+        if ($orderDelivery === null) {
+            throw new OrderDeliveryNotFoundException($orderDeliveryId);
+        }
+
+        $orderId = $orderDelivery->getOrderId();
+
+        $order = $this->getOrder($orderId,$context);
+    }
+
+
+    /**
+     * @throws OrderNotFoundException
+     */
+    private function getOrder(string $orderId, Context $context): OrderEntity
+    {
+        $orderCriteria = $this->getOrderCriteria($orderId);
+        /** @var OrderEntity|null $order */
+        $order = $this->orderRepository->search($orderCriteria, $context)->first();
+        if ($order === null) {
+            throw new OrderNotFoundException($orderId);
+        }
+
+        return $order;
+    }
+
+    private function getOrderCriteria(string $orderId): Criteria
+    {
+        $orderCriteria = new Criteria([$orderId]);
+        $orderCriteria->addAssociation('transactions');
+        return $orderCriteria;
     }
 }
