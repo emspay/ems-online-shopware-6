04b177543dc54a0f1a6c112d49902ea6dd85cd6e
tiutiunnykleasoft <alexander.tiutiunnyk@leasoft.org>
2021-06-25 15:17:11 +0300

    fixes

---
+++ src/Components/BankConfig.php
@@ -0,0 +1,10 @@
+<?php
+namespace GingerPlugin\Components;
+
+class BankConfig
+{
+    const PAYMENT_METHODS_PREFIX = "EMS Online";
+    const FILE_PREFIX = "ems_payments";
+    const API_ENDPOINT = 'https://api.online.emspay.eu';
+    const PLUGIN_TECH_PREFIX = 'emspay';
+}
